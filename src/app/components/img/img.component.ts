b7de8ac4abd7d3b63af3c7d81ec13acc64e3ed1b
andresdev <andresmestra11a@gmail.com>
2021-10-17 16:32:39 -0500

    feat(Lifecycle): Ciclo de vida de los components

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss'],
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent
+  implements OnInit, OnChanges, AfterViewInit, OnChanges, OnDestroy
+{
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imgDefault: string = './assets/images/default.png';
 
-  constructor() {}
+  constructor() {
+    //before render
+    //NO async -- once time
+    console.log('Constructor', 'imgValue', this.img);
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges() {
+    //before render
+    // change inputs -- times
+    console.log('OnChanges', 'imgValue', this.img);
+  }
+
+  ngOnInit(): void {
+    //before render
+    // async- fetch, promises -- once time
+    console.log('OnInit', 'imgValue', this.img);
+  }
+
+  ngAfterViewInit() {
+    //after render
+    //handler children
+    console.log('AfterViewInit');
+  }
+
+  ngOnDestroy() {
+    //delete component
+    console.log('OnDestroy');
+  }
 
   imgError() {
     this.img = this.imgDefault;
